ec11627a83f18b1f6a01bbe6a404021c399bc6cc
Derradji Senani <89805807+u0a254@users.noreply.github.com>
2023-02-15 21:50:56 +0100

    Update Responses.py

--- chatBot/Responses.py
+++ chatBot/Responses.py
@@ -2,30 +2,18 @@
 import telegram
 from telegram.ext import Updater, MessageHandler, Filters , CommandHandler
 import openai
-from os import system
 
 # Start Message :
 def start_command(update, context):
     start = '''
-Asalam Alaykum , السلام عليكم
-Welcome to my BOT
-You can GET ALL the Answers About :
-EveryThings You Want to Know it
-This BOT is Working With :
-ChatGPT Platform Using Openai API
-So .. Enjoy Brother ^_^
+assalamu alaikum , السلام عليكم
+welcome to my bot
+you can ask any question
+this bot is working by :
+openai api [chat_gpt]
+so .. Enjoy '-'
     '''
     update.message.reply_text(start)
-
-# Stop Message :
-def stop_command(update, context):
-    
-    stop = '''
-Thank You For Using My BOT
-See You ...
-    '''
-    update.message.reply_text(stop)
-    
 
 # Define a function to generate a response to a user message using ChatGPT :
 def generate_response(message):
